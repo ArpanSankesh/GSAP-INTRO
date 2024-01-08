2f4c0836e17a16dcacef04024e7f9ebd89ec466a
ArpanSankesh <arpansankesh830@gmail.com>
2024-01-08 20:22:29 +0530

    Learning to use timeLine

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
+const tl = gsap.timeline()
 
-
- const obj = {
-    duration:1, 
-    x:'1000',
-    backgroundColor:'#fff',
-
- }
-
- gsap.from(".box", obj);
+tl.to('h1',{
+    duration:1,
+    x:700,
+    delay:1,
+    yoyo:true
+})
